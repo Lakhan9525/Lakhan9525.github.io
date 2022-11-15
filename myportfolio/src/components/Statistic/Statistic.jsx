cc63230b35b681d6bb7ae2640e2aa6a461366d50
Lakhan9525 <lakhanraj987@gmail.com>
2022-11-15 17:15:25 +0530

    contrast

--- myportfolio/src/components/Statistic/Statistic.jsx
+++ myportfolio/src/components/Statistic/Statistic.jsx
@@ -5,30 +5,30 @@ const Statistic = () => {
   return (
     <div>
         <br /><br />
-        <div className='main-stats' style={{color:"#652B19"}}>
+        <div className='main-stats' style={{color:"rgb(255, 0, 179)"}}>
             <div>
-            <div className='circle'><p>1200+</p></div>
-            <h5>Hours</h5>
+            <div className='circle-even'><p>1200+</p></div>
+            <h5 className='text_font'>Hours</h5>
             <h6>Of Coding</h6>
             </div>
             <div>
             <div className='circle-even'><p>320+</p></div>
-            <h5>HackerRank</h5>
+            <h5 className='text_font'>HackerRank</h5>
             <h6>Points</h6>
             </div>
             <div>
-            <div className='circle'><p>400+</p> </div>
-            <h5>Git</h5>
+            <div className='circle-even'><p>400+</p> </div>
+            <h5 className='text_font'>GitHub</h5>
             <h6>Commits</h6>
             </div>
             <div>
             <div className='circle-even'><p>5</p></div>
-            <h5>Projects</h5>
+            <h5 className='text_font'>Projects</h5>
             <h6>Done</h6>
             </div>
             <div>
-            <div className='circle'><p>100+</p></div>
-            <h5>Hours</h5>
+            <div className='circle-even'><p>100+</p></div>
+            <h5 className='text_font'>Hours</h5>
             <h6>Of Soft Skills</h6>
             </div>
             
